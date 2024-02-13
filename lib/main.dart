fdda0302cce79c2bfcd9c7374ee8f878aea50a47
Joyna-Joy <joynajoy24@gmail.com>
2024-02-13 18:35:35 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MainPage());
+}
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
