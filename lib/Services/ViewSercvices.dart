0a1f7114dc6103cbf17d93aad54be4b73baecf8e
Joyna-Joy <joynajoy24@gmail.com>
2024-02-14 10:20:06 +0530

    commit

--- lib/Services/ViewSercvices.dart
+++ lib/Services/ViewSercvices.dart
@@ -3,7 +3,8 @@ import 'package:securty_app_frontend/Models/View_SecurityModel.dart';
 import 'dart:convert';
 import 'package:securty_app_frontend/Pages/view_Security.dart';
 class SecurityApiServices {
-  Future<dynamic> viewSecurityData(String userName, String employeeId,String address, String phoneNumber,String emailId,String password) async {
+  Future<dynamic> viewSecurityData(
+      String userName, String employeeId,String address, String phoneNumber,String emailId,String password) async {
     var client = http.Client();
     var apiUrl = Uri.parse("");
     var response = await client.post(apiUrl,
